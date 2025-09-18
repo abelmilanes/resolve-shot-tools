08cf146f21fad36a42f37b83f2a64a34d5ab0273
abelmilanes <abelmilanes@yahoo.com>
2025-09-17 22:39:36 -0700

    - First commit

--- proj_tools/proj_naming.py
+++ proj_tools/proj_naming.py
@@ -1,17 +1,16 @@
 import re
-from typing import Dict, Optional, Union
 from proj_tools.proj_data import Config
 
 
 class NamingConvention:
     def __init__(self):
-        self.config_data = Config().get_config()
+        self.config = Config().get_config()
 
     def naming(self, category, preset="", args_dict=None):
 
         if category == "image":
-            name = self.config_data['naming_convention'][category][preset]['file']
-            path = self.config_data['naming_convention'][category][preset]['path']
+            name = self.config['naming_convention'][category][preset]['file']
+            path = self.config['naming_convention'][category][preset]['path']
             if args_dict:
                 for k, v in args_dict.items():
                     if k in name:
@@ -21,14 +20,21 @@ class NamingConvention:
             return {"path": path, "name": name}
 
         if category == "shotid":
-            pfix = self.config_data['naming_convention'][category]['pfix']
-            pad = self.config_data['naming_convention'][category]['pad']
+            pfix = self.config['naming_convention'][category]['pfix']
+            pad = self.config['naming_convention'][category]['pad']
             shotid = [pfix, pad]
             if args_dict:
                 shotid[0] = re.sub("<sequence>", args_dict['seq'], pfix)
                 shotid[1] = str(int(args_dict['id'])).zfill(int(pad))
                 shotid = ''.join(shotid)
                 return shotid
+
+        if category == "paths":
+            path = self.config['naming_convention'][category][preset]['path']
+            if args_dict:
+                for k, v in args_dict.items():
+                    path = re.sub(k, v, path)
+                return path
 
 
 if __name__ == "__main__":
